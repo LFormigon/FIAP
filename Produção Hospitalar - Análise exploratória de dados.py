3a86b7d57d4983334012afc427c425c9bd33fadd
LFormigon <leonardofajoli2@gmail.com>
2025-05-19 21:49:51 -0300

    FIAP

--- Produção Hospitalar - Análise exploratória de dados.py
+++ Produção Hospitalar - Análise exploratória de dados.py
@@ -30,3 +30,35 @@ plt.title('Tabela da Federeixuim')
 
 #Exibindo o nome da nossa Tabela
 plt.show()
+
+#Desafio Aula 02 (Fazer uma tabela como no exercicio anterior só que com os dados mais atualizados, E deixa as unidades de federação com as letras inclinidas com algum grau)
+
+#Basicamente a Biblioteca matplotlib é uma biblioteca que nos auxilia na visualização de dados, Portanto, vamos utilizar varias vezes
+
+import matplotlib.pyplot as plt
+
+import matplotlib.ticker as ticker
+
+
+
+#Axis seria um "eixo" para as tabelas (StrMethodFormatter) seria a formatação da tabela
+
+#Kind (tipo de tabela)  figsize (tamanho da tabela)
+
+axis = dados.plot(x='Unidade da Federação', y='2021/Ago', kind='bar', figsize=(9,6))
+
+#em Axis estamos formantando a tabela para aparecer somente 2 casas após o '.' e exibindo os valores corretos apos a ','
+
+axis.yaxis.set_major_formatter(ticker.StrMethodFormatter('{x:,.2f}'))
+
+# em Axis na nossa tabela o campo "x" seria nossa unidade de federação (nome dos estados) caso eu queria deixar os nomes apresentado com alguma inclinição de grau esse seria o metado:
+
+axis.set_xticklabels(axis.get_xticklabels(), rotation=85)
+
+
+#Definindo o nome da nossa tabela
+
+plt.title('Tabela da Federeixuim')
+
+#Exibindo o nome da nossa Tabela
+plt.show()
